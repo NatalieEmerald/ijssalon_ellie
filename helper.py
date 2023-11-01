ba725fcbaa3680e7a086af61f0e7b838bb798219
Natalie <sbae021@gmail.com>
2023-11-01 19:03:16 +0100

    project

--- helper.py
+++ helper.py
@@ -1,28 +1,9 @@
-'''
-def decoreer(tekst=""):
-    tekst="header"    
-    lengte = len(tekst) + 4   
-    print()   
-    print(lengte * "*")   
-    print(f"* {tekst} *")    
-    print(lengte * "*")    
-    print()
-
-def fooi_pp(bedrag, personen):         
-    bedrag_pp = bedrag/personen            
-    return f"Het bedrag per persoon is {bedrag_pp} euro"
-
-b = int(input("Welk bedrag zit er in de fooienpot? "))
-p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
-
-print(fooi_pp(b,p))
-
 def onderstreep(tekst=""):
     uit = []
     uit.append(tekst)
     uit.append(len(tekst) * "=")
     return uit
-'''
+
 def som(inkomsten):
     totaal = sum(inkomsten.values())
     return totaal
--- presentatie.py
+++ presentatie.py
@@ -4,10 +4,9 @@ def presenteer(mijn_dict, totaal):
     print("==========================")
     print(f"Totaal : {totaal} euro")
 
-'''
+
 mijn_dict = {'vis' : 10, 'vlees' : 25, 'overig' : 15}
 totaal = 50
 
 presenteer(mijn_dict, totaal)
-'''
 
